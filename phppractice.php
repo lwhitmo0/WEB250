702fabdbf9640998b2a123302d14d41a6154cff1
lwhitmo0 <148815855+lwhitmo0@users.noreply.github.com>
2024-10-02 12:02:33 -0400

    Update phppractice.php

--- phppractice.php
+++ phppractice.php
@@ -1,8 +1,8 @@
-○	$sOutputString = "<hr/>";
-○	
-○	for($iCounter = 1; $iCounter <= 1000; $iCounter++)
-○	{
-○	     $sOutputString .= $iCounter . ") Whitmore got PHP running on the computer! Sheesh! ";
-○	}
-○	
-○	echo ("<h5>" . $sOutputString . "</h5><hr/>");
+<?php
+$sOutputString = "<hr/>;
+for($iCounter = 1; $iCounter <= 1000; $iCounter++)
+{
+     $sOutputString .= $iCounter . ") Whitmore got PHP running on the computer! Sheesh! ";
+}
+
+echo ("<h5>" . $sOutputString . "</h5><hr/>");
